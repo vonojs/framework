fa4ab34d4b6d8e6d46e53d73527899f4dbd2d620
benton <ben@benten.ca>
2025-10-20 02:13:27 -0700

    export nitro from lib/nitro

--- src/virtual/server.ts
+++ src/virtual/server.ts
@@ -1,6 +1,5 @@
 export default (devEntry: string) => `
-export * from "nitro/h3"
-export * from "nitro/runtime" 
+export * from "@vonojs/framework/lib/nitro" 
 import manifest from "virtual:vite-manifest"
 export const clientEntry = import.meta.env.DEV ? "${devEntry}" : "/" + Object.values(manifest).find(m => m.isEntry).file;
 export const css = import.meta.env.DEV ? [] : Object.values(manifest).find(m => m.isEntry).css.map(c => "/"+c);
