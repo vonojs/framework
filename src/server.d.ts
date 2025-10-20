94a49c67a8fadc3f9474a443ae9ea9e4b24d19db
benton <ben@benten.ca>
2025-10-20 02:57:24 -0700

    export manifest from server file

--- src/server.d.ts
+++ src/server.d.ts
@@ -3,4 +3,5 @@ declare module "@vonojs/framework/server" {
 	export * from "nitro/runtime"
 	export const css: string[]
 	export const clientEntry: string
+	export const manifest: import("vite").Manifest
 }
--- src/virtual/server.ts
+++ src/virtual/server.ts
@@ -1,6 +1,8 @@
 export default (devEntry: string, build: boolean) => `
-${!build ? "export * from 'nitro/runtime'; export * from 'nitro/h3';" : `export * from "@vonojs/framework/lib/nitro";` }
+export * from 'nitro/runtime';
+export * from 'nitro/h3';
 import manifest from "virtual:vite-manifest"
+export const manifest = manifest;
 export const clientEntry = import.meta.env.DEV ? "${devEntry}" : "/" + Object.values(manifest).find(m => m.isEntry).file;
 export const css = import.meta.env.DEV ? [] : Object.values(manifest).find(m => m.isEntry).css.map(c => "/"+c);
 `
