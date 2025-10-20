e99840d6e1d74ff4c175c2f5cde702d4de5a1145
benton <ben@benten.ca>
2025-10-20 02:34:39 -0700

    fix errant semicolon

--- src/virtual/server.ts
+++ src/virtual/server.ts
@@ -1,5 +1,5 @@
 export default (devEntry: string, build: boolean) => `
-${!build ? "export * from 'nitro/runtime'; export * from 'nitro/h3';" : `export * from "@vonojs/framework/lib/nitro;"` }
+${!build ? "export * from 'nitro/runtime'; export * from 'nitro/h3';" : `export * from "@vonojs/framework/lib/nitro";` }
 import manifest from "virtual:vite-manifest"
 export const clientEntry = import.meta.env.DEV ? "${devEntry}" : "/" + Object.values(manifest).find(m => m.isEntry).file;
 export const css = import.meta.env.DEV ? [] : Object.values(manifest).find(m => m.isEntry).css.map(c => "/"+c);
