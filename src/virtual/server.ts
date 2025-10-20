b2b9673c1b76ddd01279d1b4bda45fc323020a81
benton <ben@benten.ca>
2025-10-20 03:01:14 -0700

    fix manifest export dupe

--- src/virtual/server.ts
+++ src/virtual/server.ts
@@ -1,8 +1,8 @@
 export default (devEntry: string, build: boolean) => `
 export * from 'nitro/runtime';
 export * from 'nitro/h3';
-import manifest from "virtual:vite-manifest"
-export const manifest = manifest;
+import _manifest from "virtual:vite-manifest"
+export const manifest = _manifest;
 export const clientEntry = import.meta.env.DEV ? "${devEntry}" : "/" + Object.values(manifest).find(m => m.isEntry).file;
 export const css = import.meta.env.DEV ? [] : Object.values(manifest).find(m => m.isEntry).css.map(c => "/"+c);
 `
